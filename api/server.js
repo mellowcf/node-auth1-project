ba68b947311d7b3b84546ad3ef3d02de79d7a9f1
Sterling Ford <mellowcf@gmail.com>
2021-03-01 22:44:27 -0500

    mvp

--- api/server.js
+++ api/server.js
@@ -1,35 +1,41 @@
 const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
+const session = require("express-session");
+const KnexStore = require("connect-session-knex")(session); 
 
-/**
-  Do what needs to be done to support sessions with the `express-session` package!
-  To respect users' privacy, do NOT send them a cookie unless they log in.
-  This is achieved by setting 'saveUninitialized' to false, and by not
-  changing the `req.session` object unless the user authenticates.
-
-  Users that do authenticate should have a session persisted on the server,
-  and a cookie set on the client. The name of the cookie should be "chocolatechip".
-
-  The session can be persisted in memory (would not be adecuate for production)
-  or you can use a session store like `connect-session-knex`.
- */
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const restricted = require("../auth/restricted-middleware.js");
+const knex = require("../database/dbConfig.js"); 
 
 const server = express();
 
-server.use(helmet());
+const sessionConfig = {
+  name: "Authorize Me",
+  secret: "keep it secret, keep it safe!",
+  resave: false,
+  saveUninitialized: true, 
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true,
+  },
+  store: new KnexStore({
+    knex,
+    tablename: "sessions",
+    createtable: true,
+    sidfieldname: "sid",
+    clearInterval: 1000 * 60 * 15,
+  }),
+};
+
+
 server.use(express.json());
-server.use(cors());
+server.use(session(sessionConfig)); 
+server.use("/api/auth", authRouter);
+server.use("/api/users", restricted, usersRouter);
 
 server.get("/", (req, res) => {
-  res.json({ api: "up" });
+  res.json({ api: "Welcome" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
-  res.status(500).json({
-    message: err.message,
-    stack: err.stack,
-  });
-});
-
-module.exports = server;
+module.exports = server; 
--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,44 @@
-// DO NOT CHANGE THIS FILE
-const sharedConfig = {
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  migrations: {
-    directory: './data/migrations',
-  },
-  seeds: {
-    directory: './data/seeds',
-  },
-  // this enables foreign keys in SQLite
-  pool: {
-    afterCreate: (conn, done) => {
-      conn.run('PRAGMA foreign_keys = ON', done)
-    },
-  },
-}
+// Update with your config settings.
 
 module.exports = {
+
   development: {
-    ...sharedConfig,
-    connection: { filename: './data/auth.db3' },
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
   },
-  testing: {
-    ...sharedConfig,
-    connection: { filename: './data/testing.db3' },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
-}
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
